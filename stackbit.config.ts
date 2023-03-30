382213908f8cc207a49ed174b8397a797c19f97f
Sean C Davis <scdavis41@gmail.com>
2023-03-30 12:08:22 -0400

    Add node version

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -4,6 +4,7 @@ import { defineStackbitConfig } from '@stackbit/types';
 export default defineStackbitConfig({
     stackbitVersion: '~0.6.0',
     ssgName: 'gatsby',
+    nodeVersion: '18',
     contentSources: [
         new ContentfulContentSource({
             spaceId: process.env.CONTENTFUL_SPACE_ID!,
